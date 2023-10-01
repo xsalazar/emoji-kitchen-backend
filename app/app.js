bb9f8548321e1d1d3ed2451252b9afaec8af036d
Xavier Salazar <x.salazar1729@gmail.com>
2023-10-01 16:10:41 -0700

    Add support for bulk downloads

--- app/app.js
+++ app/app.js
@@ -6,6 +6,29 @@ exports.handler = async (event, context) => {
   console.log(JSON.stringify(event));
   const bucketName = "xsalazar-emoji-kitchen-data";
 
+  // Handle bulk download requests
+  if (
+    event.queryStringParameters &&
+    event.queryStringParameters.imageSource &&
+    event.queryStringParameters.imageSource.startsWith(
+      "https://www.gstatic.com/android/keyboard/emojikitchen"
+    ) &&
+    event.requestContext.http.method === "GET"
+  ) {
+    // Get image from internet
+    var response = await axios.get(imageSource, {
+      responseType: "arraybuffer",
+    });
+
+    return {
+      cookies: [],
+      isBase64Encoded: true,
+      statusCode: 200,
+      headers: { "content-type": "application/octet-stream" },
+      body: Buffer.from(response.data, "binary").toString("base64"),
+    };
+  }
+
   if (event.requestContext.http.method === "GET") {
     const s3 = new AWS.S3();
 
