f74c1c31883e9046edf835ff3f8cc9795b44d3d7
Xavier Salazar <contact@xsalazar.com>
2026-02-03 21:31:40 -0800

    Add `/search` API handler

---
+++ search/app.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+exports.handler = async (event, context) => {
+  console.log(JSON.stringify(event));
+
+  // Handle search queries
+  if (
+    event.queryStringParameters &&
+    event.queryStringParameters.q &&
+    event.requestContext.http.method === "GET"
+  ) {
+    const query = event.queryStringParameters.q;
+
+    // Some sanity checks
+    if (!query || query.trim() === 0 || query.length > 128) {
+      return;
+    }
+
+    const metadata = JSON.parse(fs.readFileSync(`./keywords.json`));
+
+    const emojis = Object.values(metadata.data);
+    const results = emojis
+      .filter(
+        (e) =>
+          e.alt.includes(query) || // If alt contains the query
+          e.emoji.includes(query) || // Is emoji
+          e.keywords.some((keyword) => keyword.includes(query)), // Any keywords contains query
+      )
+      .map((r) => r.emojiCodepoint);
+
+    return {
+      cookies: [],
+      isBase64Encoded: false,
+      statusCode: 200,
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(results),
+    };
+  }
+};
